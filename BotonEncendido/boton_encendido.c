cdd30f80a8e32282d35eb2b85425cb7c1f5a4bf1
Rodrigo Zamudio Tovar <royzamudiotovar@gmail.com>
2024-04-10 17:12:56 -0600

    Inicialización de funciones

--- BotonEncendido/boton_encendido.c
+++ BotonEncendido/boton_encendido.c
@@ -6,6 +6,20 @@
 #define LED_PIN 2
 
 int main() {
+    // Inicialización del sistema de entrada/salida estándar
     stdio_init_all();
+
+    // Inicialización del pin del botón como entrada con pull-up
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);
+
+    // Inicialización del pin del LED como salida
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    while(true){
+
+    }
 
 }
