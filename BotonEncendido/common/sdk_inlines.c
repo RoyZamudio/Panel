15f69791ee8954618d3f4674e1d5a940811a0426
Rodrigo Zamudio Tovar <royzamudiotovar@gmail.com>
2024-04-10 19:37:31 -0600

    Declaración funciones inline

---
+++ BotonEncendido/common/sdk_inlines.c
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "hardware/gpio.h"
+
+void gpio_set_dir_wrapper(unsigned gpio, bool out){
+    gpio_set_dir(gpio, out);
+}
+
+void gpio_pull_up_wrapper(unsigned gpio){
+    gpio_pull_up(gpio);
+}
+
+void gpio_put_wrapper(unsigned gpio, bool out){
+    gpio_put(gpio, out);
+}
