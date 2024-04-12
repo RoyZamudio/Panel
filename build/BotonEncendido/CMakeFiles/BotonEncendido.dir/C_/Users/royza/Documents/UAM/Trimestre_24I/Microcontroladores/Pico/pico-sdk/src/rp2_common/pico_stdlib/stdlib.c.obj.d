4f1436e10433823b2b16ef4fdc2145d93819d28c
Rodrigo Zamudio Tovar <royzamudiotovar@gmail.com>
2024-04-12 12:22:45 -0600

    Añadir mensajes printf

--- build/BotonEncendido/CMakeFiles/BotonEncendido.dir/C_/Users/royza/Documents/UAM/Trimestre_24I/Microcontroladores/Pico/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c.obj.d
+++ build/BotonEncendido/CMakeFiles/BotonEncendido.dir/C_/Users/royza/Documents/UAM/Trimestre_24I/Microcontroladores/Pico/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c.obj.d
@@ -50,10 +50,14 @@ BotonEncendido\CMakeFiles\BotonEncendido.dir\C_\Users\royza\Documents\UAM\Trimes
  C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\rp2040\hardware_structs\include/hardware/structs/uart.h \
  C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/uart.h \
  C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/dreq.h \
- C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\rp2_common\pico_stdio_uart\include/pico/stdio_uart.h \
+ C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\rp2_common\pico_stdio_usb\include/pico/stdio_usb.h \
  C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\rp2_common\hardware_pll\include/hardware/pll.h \
  C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\rp2040\hardware_structs\include/hardware/structs/pll.h \
  C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/pll.h \
  C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\rp2_common\hardware_clocks\include/hardware/clocks.h \
  C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\rp2040\hardware_structs\include/hardware/structs/clocks.h \
- C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/clocks.h
+ C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/clocks.h \
+ C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\common\pico_binary_info\include/pico/binary_info.h \
+ C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\common\pico_binary_info\include/pico/binary_info/defs.h \
+ C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\common\pico_binary_info\include/pico/binary_info/structure.h \
+ C:\Users\royza\Documents\UAM\Trimestre_24I\Microcontroladores\Pico\pico-sdk\src\common\pico_binary_info\include/pico/binary_info/code.h
